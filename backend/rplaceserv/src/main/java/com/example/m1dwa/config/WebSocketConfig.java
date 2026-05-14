4ebb7b899626c72496765f540a42c2907cf766c8
TitouanCharrier <70266642+TitouanCharrier@users.noreply.github.com>
2026-05-14 21:05:04 +0200

    feat: ajout de la config de deploy

--- backend/rplaceserv/src/main/java/com/example/m1dwa/config/WebSocketConfig.java
+++ backend/rplaceserv/src/main/java/com/example/m1dwa/config/WebSocketConfig.java
@@ -24,12 +24,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                .setAllowedOrigins( "https://serv-dwa.charrier.ovh", "http://localhost:5173", "http://127.0.0.1:5173")
                 .withSockJS();
         
         //sans SockJS pour des tests (à enlever plus tard)
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173", "http://127.0.0.1:5173");
+        //registry.addEndpoint("/ws")
+        //        .setAllowedOrigins("http://localhost:5173", "http://127.0.0.1:5173");
     }
 
     @Override
